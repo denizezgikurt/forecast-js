029eb7df688e8845965fa5d514e5c426decb086e
Deniz Ezgi Kurt <deniz.ezgi.kurt@epiphany-rbc.com>
2020-08-30 21:12:06 +0200

    calculating my Age in dog years

---
+++ dogyears-calculator/app.js
@@ -0,0 +1,23 @@
+// my age is 30
+const myAge = 30;
+
+//tearly years. his value will change
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+const myName = 'Deniz'.toLowerCase();
+
+console.log(myName);
+console.log(myAgeInDogYears);
+
+console.log(`My name is ${myName}, I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years`);
